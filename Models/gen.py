f34625ca690ef5432c72cb66117bbee20a25a0b3
Ziad-Tari <tarikziad347@gmail.com>
2025-11-22 17:06:09 -0500

    Doing the strcutre of the project and GEN model

---
+++ Models/gen.py
@@ -0,0 +1,50 @@
+# Generator Code
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from Conifg import config as cfg
+from IPython.display import HTML
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( cfg.nz, cfg.ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(cfg.ngf * 8),
+            nn.ReLU(True),
+            # state size. ``(ngf*8) x 4 x 4``
+            nn.ConvTranspose2d(cfg.ngf * 8, cfg.ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(cfg.ngf * 4),
+            nn.ReLU(True),
+            # state size. ``(ngf*4) x 8 x 8``
+            nn.ConvTranspose2d( cfg.ngf * 4, cfg.ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(cfg.ngf * 2),
+            nn.ReLU(True),
+            # state size. ``(ngf*2) x 16 x 16``
+            nn.ConvTranspose2d( cfg.ngf * 2, cfg.ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(cfg.ngf),
+            nn.ReLU(True),
+            # state size. ``(ngf) x 32 x 32``
+            nn.ConvTranspose2d( cfg.ngf, cfg.nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. ``(nc) x 64 x 64``
+        )
+
+    def forward(self, input):
+     if isinstance(input, torch.cuda.FloatTensor) and self.ngpu > 1:
+        return nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+     else:
+        return self.main(input)
